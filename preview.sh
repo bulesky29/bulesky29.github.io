e328f997f21bf3a6ec9411c19ebaacd268b4d02e
王天翼 <wangtianyi@4paradigm.com>
2025-06-17 14:09:13 +0800

    test action

--- preview.sh
+++ preview.sh
@@ -1,10 +1,18 @@
 mkdir -p tianyi_site/content/post
-mkdir -p tianyi_site/content/archive
+mkdir -p tianyi_site/content/archives
+mkdir -p tmp
 \rm -r tianyi_site/content/post
-\rm -r tianyi_site/content/archive
+\rm -r tianyi_site/content/archives
+\rm -r tmp
 mkdir -p tianyi_site/content/post
-mkdir -p tianyi_site/content/archive
+mkdir -p tianyi_site/content/archives
+mkdir -p tmp
 hugo mod tidy --source tianyi_site
-obsidian-export --ignore archive vaults/blog tianyi_site/content/post
-obsidian-export vaults/blog/archive tianyi_site/content/archive
+cp -r vaults/blog tmp
+rm -r tmp/blog/00-template
+obsidian-export tmp/blog tianyi_site/content/post
+mv tianyi_site/content/post/archives/* tianyi_site/content/archives
+rm -r tianyi_site/content/post/archives
+mv tianyi_site/content/post/about.md tianyi_site/content
+mv tianyi_site/content/post/friend.md tianyi_site/content
 hugo server --source tianyi_site -D
---
+++ tianyi_site/content/archives/link_test.md
@@ -0,0 +1,7 @@
+---
+date: 2025-06-17T12:00:00+09:00
+title: 归档测试
+---
+
+test
+[Welcome](../Welcome.md)
